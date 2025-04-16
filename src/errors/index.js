d63ba24c8d5d9cda895a977ac5e239e6dcc217e2
kwildeDev <73887333+kwildeDev@users.noreply.github.com>
2025-04-16 21:34:36 +0100

    register an attendee for an event by event id

--- src/errors/index.js
+++ src/errors/index.js
@@ -1,8 +1,11 @@
 exports.psqlErrorHandler = (err, request, response, next) => {
-    if (err.code === "23502" || err.code === "22P02" || err.code === "23503") {
+    if (err.code === "23502" || err.code === "22P02") {
         response.status(400).send({ msg: "Bad Request" });
-      } else next(err);
-    next(err)
+    } else if (err.code === "23503") {
+        response.status(404).send({ msg: "Not Found" });
+    } else {
+        next(err);
+    }
 };
 
 exports.customErrorHandler = (err, request, response, next) => {
