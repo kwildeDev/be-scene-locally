5fb537d67ce36b38ad735970bdd4c69c6bad19a3
kwildeDev <73887333+kwildeDev@users.noreply.github.com>
2025-04-20 20:10:57 +0100

    implement filtering of events by subcategory

--- src/models/subcategories-models.js
+++ src/models/subcategories-models.js
@@ -10,3 +10,16 @@ exports.fetchSubcategoriesByCategory = (categoryId) => {
             return rows;
         });
 };
+
+exports.fetchSubcategoryIdBySlug = (category_id, subcategorySlug) => {
+    return db
+        .query(`SELECT subcategory_id FROM subcategories WHERE category_id = $1 AND slug = $2;`, [
+            category_id, subcategorySlug,
+        ])
+        .then(({ rows }) => {
+            if (rows.length === 0) {
+                return Promise.reject({ status: 404, msg: 'Subcategory Not Found'})
+            }
+            return rows[0] || null;
+        });
+};
