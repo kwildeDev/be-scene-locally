eaa7a2bd102982f5409b30683183fc19c69e5756
kwildeDev <73887333+kwildeDev@users.noreply.github.com>
2025-05-28 20:28:27 +0100

    fetch category, subcategory and subcategories by id

--- src/models/subcategories-models.js
+++ src/models/subcategories-models.js
@@ -11,11 +11,26 @@ exports.fetchSubcategoriesByCategory = (categoryId) => {
         });
 };
 
+// used for events
 exports.fetchSubcategoryIdBySlug = (category_id, subcategorySlug) => {
     return db
         .query(`SELECT subcategory_id FROM subcategories WHERE category_id = $1 AND slug = $2;`, [
             category_id, subcategorySlug,
         ])
+        .then(({ rows }) => {
+            if (rows.length === 0) {
+                return Promise.reject({ status: 404, msg: 'Subcategory Not Found'})
+            }
+            return rows[0] || null;
+        });
+};
+
+exports.fetchSubcategoryById = (subcategoryId) => {
+    return db
+        .query(
+            `SELECT category_id, subcategory_id, name, slug, description FROM subcategories WHERE subcategory_id = $1;`,
+            [subcategoryId]
+        )
         .then(({ rows }) => {
             if (rows.length === 0) {
                 return Promise.reject({ status: 404, msg: 'Subcategory Not Found'})
